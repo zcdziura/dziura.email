762f95a16f8b7cb08dfcb5749272139694e4b133
Z. Charles Dziura <zachary@dziura.email>
2022-03-25 08:58:58 -0400

    Add animation to skills progress bars

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 !(function () {
+  /*
+   * NAVIGATION LINKS
+   */
   const navLinks = document.querySelectorAll("nav > ul > li");
   navLinks.forEach((link) => {
     link.addEventListener("click", ({ target }) => {
       const id = target.text.toLowerCase();
-      setNavLinkAsActive(navLinks, target);
       scrollToElement(id);
     });
   });
-
-  function setNavLinkAsActive(navLinks, link) {
-    navLinks.forEach((l) => {
-      l.classList.remove("active");
-      link.parentElement.classList.add("active");
-    });
-  }
 
   function scrollToElement(id) {
     const element = document.getElementById(id);
@@ -21,4 +16,47 @@
       behavior: "smooth",
     });
   }
+
+  const intersectionObserverOptions = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.5,
+  };
+
+  function navIntersectionObserverCallback(entries) {
+    entries.forEach(({ isIntersecting, target }) => {
+      const id = `nav-${target.id.toLowerCase()}`;
+      const navItem = document.getElementById(id);
+
+      if (isIntersecting) {
+        navItem.classList.add("active");
+      } else {
+        navItem.classList.remove("active");
+      }
+    });
+  }
+
+  const navObserver = new IntersectionObserver(navIntersectionObserverCallback, intersectionObserverOptions);
+  document.querySelectorAll("section").forEach((section) => {
+    navObserver.observe(section);
+  });
+
+  function skillsIntersectionObserverCallback(entries) {
+    const progressBars = document.querySelectorAll(".progress-bar");
+    entries.forEach(({ isIntersecting }) => {
+      if (isIntersecting) {
+        progressBars.forEach((progressBar) => {
+          const widthPercent = parseInt(progressBar.getAttribute("aria-valuenow")) * 10;
+          progressBar.style.width = `${widthPercent}%`;
+        });
+      } else {
+        progressBars.forEach((progressBar) => {
+          progressBar.style.width = "0";
+        });
+      }
+    });
+  }
+
+  const skillsObserver = new IntersectionObserver(skillsIntersectionObserverCallback, intersectionObserverOptions);
+  skillsObserver.observe(document.getElementById("skills"));
 })();
